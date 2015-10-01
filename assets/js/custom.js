bb5cd992522ceb681b9a6b1cadd6160d6eba89f6
Abhishek Bhardwaj <abbhardwaj@paypal.com>
2015-10-01 12:20:09 +0530

    Removing tweet function

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -146,15 +146,6 @@ $("#owl-carousel-shots-phone").owlCarousel({
 ============================================= */
 $('.venobox').venobox();
 
-/* ===================================================================
-    TWEETIE -  TWITTER FEED PLUGIN THAT WORKS WITH NEW Twitter 1.1 API
-==================================================================== */
-$('.tweet').twittie({
-    apiPath : 'assets/js/plugins/twitter/api/tweet.php',
-    count: 2,
-    template: '{{tweet}} - <span class="date">{{date}}</span>'
-});
-
 
 /* =================================
    SCROLL TO                  
